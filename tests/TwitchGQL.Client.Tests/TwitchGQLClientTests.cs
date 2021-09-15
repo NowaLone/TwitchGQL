8f7891f9fb76ae05656052de9f799eecb5b4aa82
Nikita <fihs@outlook.com>
2021-09-15 23:43:15 +0200

    test: add first test

--- tests/TwitchGQL.Client.Tests/TwitchGQLClientTests.cs
+++ tests/TwitchGQL.Client.Tests/TwitchGQLClientTests.cs
@@ -1,9 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace TwitchGQL.Client.Tests
 {
     [TestClass]
     public class TwitchGQLClientTests
     {
+        private const string testLogin = "monstercat";
+        private readonly ITwitchGQLClient twitchGQLClient;
+
+        public TwitchGQLClientTests()
+        {
+            twitchGQLClient = new TwitchGQLClient
+            {
+                ClientId = "kimne78kx3ncx6brgo4mv6wki5h1ko"
+            };
+        }
+
+        [TestMethod]
+        public async Task SendQueryAsync_PlaybackAccessTokenRequest_ShouldReturnData()
+        {
+            // arrange
+            Models.Requests.Templates.PlaybackAccessTokenRequest request = new(login: testLogin);
+
+            // act
+            var data = await twitchGQLClient.SendQueryAsync(request).ConfigureAwait(false);
+
+            // assert
+            Assert.IsNotNull(data);
+            Assert.IsNotNull(data.StreamPlaybackAccessToken);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(data.StreamPlaybackAccessToken.Value));
+            Assert.IsFalse(string.IsNullOrWhiteSpace(data.StreamPlaybackAccessToken.Signature));
+        }
     }
 }
